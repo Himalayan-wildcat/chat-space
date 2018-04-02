6a8a265ab53540780f7553066babb3a5666043eb
Himalayan-wildcat <npng.h.0709@gmail.com>
2018-04-02 12:12:47 +0900

    [WIP]chat-group-making(ver.3)
    run a group-list program

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,14 @@
 
 
 class GroupsController < ApplicationController
+    before_action :set_group, only: [:edit, :update]
+
   def new
     @group = Group.new
     @group.users << current_user
   end
 
   def create
-    # binding.pry
     @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
@@ -16,14 +17,20 @@ class GroupsController < ApplicationController
     end
   end
 
-  def edit
-  end
-
   def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'group edited successfully!!'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
   def update
     if current_user.update(user_params)
       redirect_to root_path
-    # redirect_to root_path unless current_user.update(user_params)
     else
       render :edit
     end
